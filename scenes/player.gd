d91248e28362b2429e4865fd9a5738bea092153a
luigi gomes <luigi.gomes@epitech.eu>
2026-02-06 22:15:56 +0100

    fix(player double jump): Fixing player double jump

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,17 +3,23 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
-
+const CURRENT_MAX_JUMP = 2
+var MAX_JUMPS = CURRENT_MAX_JUMP
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	# setting the nb of jump to basic
+	if is_on_floor():
+		MAX_JUMPS = CURRENT_MAX_JUMP
 
-	# Handle jump .
-	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	# Handle double jump 
+	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up"):
+		if MAX_JUMPS > 0:
+			velocity.y = JUMP_VELOCITY
+			MAX_JUMPS -= 1
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
