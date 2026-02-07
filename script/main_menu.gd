9cfbb7c2b5577311ea21889b243f138c9f83e488
Kerwanc <kerwan.c@gmail.com>
2026-02-07 18:13:23 +0100

    fix(menu): switch main menu object to a control object

--- script/main_menu.gd
+++ script/main_menu.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
@@ -10,3 +10,4 @@ func _on_option_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+!
